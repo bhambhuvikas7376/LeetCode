01b40718a2e4b41d3cb85694d9d21b5d78486e3c
VIKAS BHAMBHU <vikasbhambhu73@gmail.com>
2024-04-25 16:23:54 +0530

    0028-find-the-index-of-the-first-occurrence-in-a-string.java

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,4 +1,21 @@
 class Solution {
+  public int strStr(String haystack, String needle) {
+    final int m = haystack.length();
+    final int n = needle.length();
+
+    for (int i = 0; i < m - n + 1; ++i)
+      if (haystack.substring(i, i + n).equals(needle))
+        return i;
+
+    return -1;
+  }
+}
+
+
+------------------------------------------------------------------------
+
+    
+class Solution {
     public int strStr(String haystack, String needle) {
         int ans=0;
         int count=0;
