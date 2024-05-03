a4a18dcb31a6923bc000ad86ef3abb3261f78789
VIKAS BHAMBHU <vikasbhambhu73@gmail.com>
2024-05-03 15:25:13 +0530

    0176-second-highest-salary.sql

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
 select ifnull(max(salary),null) as SecondHighestSalary from Employee where salary not in (select max(salary) from Employee) 
+
+
+
+select IFNULL((select distinct Salary from Employee order by Salary desc limit 1, 1), NULL) SecondHighestSalary;
